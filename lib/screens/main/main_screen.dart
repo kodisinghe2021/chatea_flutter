aa86d2a926795ae5664d46051a3dd3fa03334bb0
kodisinge2021 <fxkodisinge@gmail.com>
2023-03-16 13:23:46 +0530

    functional_begining

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -4,6 +4,8 @@ import 'package:chatea_app/common/components/custom_buttons.dart';
 import 'package:chatea_app/common/components/custom_dialog_box.dart';
 import 'package:chatea_app/common/components/custom_images.dart';
 import 'package:chatea_app/common/components/custom_text_field.dart';
+import 'package:chatea_app/screens/main/chat/chat.dart';
+import 'package:chatea_app/screens/main/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 
@@ -15,71 +17,19 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  bool isLoading = false;
+  // current screen index
+  int currentIndex = 0;
+
+  //screens list
+  final List<Widget> _screens = [
+    const Home(),
+    const Chat(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("hi .. prabhath"),
-      ),
-      body: SizedBox(
-        width: screenSize(context).width,
-        height: screenSize(context).height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomButton(
-              onTap: () {
-                Logger().i("Clicked");
-              },
-              text: 'click',
-            ),
-            const SizedBox(height: 30),
-            SizedBox(
-              width: screenSize(context).width * .8,
-              child: CustomTextField(),
-            ),
-            const SizedBox(height: 30),
-            const SvgIcon(imgPath: 'users-ico'),
-            const SizedBox(height: 20),
-            networkImage(
-              'https://png.pngtree.com/png-clipart/20190924/original/pngtree-user-vector-avatar-png-image_4830521.jpg',
-            ),
-            const SizedBox(height: 30),
-            networkImageCirculler(
-              'https://png.pngtree.com/png-clipart/20190924/original/pngtree-user-vector-avatar-png-image_4830521.jpg',
-            ),
-            const SizedBox(height: 20),
-            CustomButton(
-              isLoading: isLoading,
-              text: 'show message',
-              onTap: () {
-                setState(() {
-                  isLoading = true;
-                });
-                DialogBox().dialogBox(
-                  context,
-                  DialogType.info,
-                  'title',
-                  'desc',
-                  () {
-                    Logger().e('CANCEL');
-                    setState(() {
-                      isLoading = false;
-                    });
-                  },
-                  () {
-                    Logger().e('OK');
-                    setState(() {
-                      isLoading = false;
-                    });
-                  },
-                );
-              },
-            ),
-          ],
-        ),
-      ),
+      body: _screens.elementAt(currentIndex),
     );
   }
 }
